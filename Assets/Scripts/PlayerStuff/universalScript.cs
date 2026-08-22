85faad6b507332c915c614dc516735cb64560365
murgi922 <murgideshi922@gmail.com>
2026-08-22 18:36:03 +0600

    Backup

--- Assets/Scripts/PlayerStuff/universalScript.cs
+++ Assets/Scripts/PlayerStuff/universalScript.cs
@@ -7,10 +7,4 @@ public class universalScript : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
