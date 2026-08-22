a59f702d9a963cc2f00419dce0c7f3dbdc92d27a
murgi922 <murgideshi922@gmail.com>
2026-08-22 17:53:21 +0600

    Backup

---
+++ Assets/Scripts/PlayerStuff/universalScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class universalScript : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
